36d39a292c299d3ab19199ea47c23232db5a2b8d
Srishti Gupta <srishti@M4s-Mac-mini.local>
2025-06-17 18:36:41 +0530

    added user my-play page

--- app/(authenticated)/my-play.tsx
+++ app/(authenticated)/my-play.tsx
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import { useGetOpenplayBookings } from '@/hooks/apis/bookings/useGetOpenplayBookings';
+import OpenPlayBookingCards from '@/components/my-play/OpenPlayBookingsPage';
+import { RootState } from '@/store';
 
 export default function MyPlay() {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const userId = user?.userId;
+
+  const {
+    data: clubBookingData = [],
+    status,
+    refetch,
+  } = useGetOpenplayBookings(userId ?? '');
+
+  console.log('clubBookingData:', clubBookingData);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>🏡 Welcome to MyPlay!</Text>
+      <OpenPlayBookingCards
+        userId={userId ?? ''}
+        bookings={clubBookingData ?? []}
+        status={status}
+        refetch={refetch}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  text: { fontSize: 24 },
+  container: {
+    flex: 1,
+    padding: 12,
+    paddingBottom: 40,
+  },
 });
