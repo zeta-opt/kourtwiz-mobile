063f39affe90fe9737685ec75d50345f2d78436f
Uday Harshith <uday.h@zetaopt.com>
2025-06-25 14:27:37 +0530

    signup fix

--- components/signup/api/api.ts
+++ components/signup/api/api.ts
@@ -15,11 +15,11 @@ export const validatePhoneOtp = (phone: string, otp: string) =>
   });
 
 export const sendEmailOtp = (email: string) =>
-  axios.post(`${BASE_URL}/temp-otp/send-email`, null, {
+  axios.post(`${BASE_URL}/otp/send-email`, null, {
     params: { email },
   });
 
 export const validateEmailOtp = (email: string, otp: string) =>
-  axios.post(`${BASE_URL}/temp-otp/validate`, null, {
+  axios.post(`${BASE_URL}/otp/validate`, null, {
     params: { recipient: email, otp },
   });
