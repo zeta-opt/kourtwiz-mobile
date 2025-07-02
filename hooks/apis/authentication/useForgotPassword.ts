f0620fbe7bee732841ae6abbab54b1f00d8b83e6
Uday Harshith <uday.h@zetaopt.com>
2025-07-02 12:13:33 +0000

    Squashed commit of the following
    commit bdfac083 
    Author: Uday Harshith <uday.h@zetaopt.com> 
    Date: Wed Jul 02 2025 17:41:54 GMT+0530 (India Standard Time) 
    
        implemented forgot password functionality

---
+++ hooks/apis/authentication/useForgotPassword.ts
@@ -0,0 +1,38 @@
+import Constants from 'expo-constants';
+import { useState } from 'react';
+import axios from 'axios';
+
+export const useForgotPassword = () => {
+  const [status, setStatus] = useState<'idle' | 'loading' | 'success' | 'error'>('idle');
+  const [error, setError] = useState<string | null>(null);
+
+  const forgotPassword = async (
+    email: string,
+    {
+      onSuccess,
+      onError,
+    }: {
+      onSuccess?: (resData: any) => void;
+      onError?: (err: any) => void;
+    } = {}
+  ) => {
+    try {
+      setStatus('loading');
+      const BASE_URL = Constants.expoConfig?.extra?.apiUrl;
+
+      const response = await axios.post(`${BASE_URL}/password/forgot-password`, null, {
+        params: { email },
+      });
+
+      setStatus('success');
+      onSuccess?.(response.data);
+    } catch (err: any) {
+      setStatus('error');
+      setError(err.message);
+      console.error('Forgot password failed:', err);
+      onError?.(err);
+    }
+  };
+
+  return { forgotPassword, status, error };
+};
