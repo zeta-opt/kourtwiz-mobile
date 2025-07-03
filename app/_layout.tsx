0ddd1815d08a7a1d87bd077525e50c392c97fae4
Srishti Gupta <srishti.g@zetaopt.com>
2025-07-03 13:46:27 +0000

    fixed back button issue and modified the clocks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,24 @@
 import { Slot } from 'expo-router';
 import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import { Provider as ReduxProvider } from 'react-redux';
-import { store } from '../store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../store';
 
 export default function RootLayout() {
   return (
     <ReduxProvider store={store}>
-      <PaperProvider>
-        <>
-          <Slot />
-          <Toast />
-        </>
-      </PaperProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <PaperProvider>
+          <SafeAreaProvider>
+            <>
+              <Slot />
+              <Toast />
+            </>
+          </SafeAreaProvider>
+        </PaperProvider>
+      </PersistGate>
     </ReduxProvider>
   );
 }
