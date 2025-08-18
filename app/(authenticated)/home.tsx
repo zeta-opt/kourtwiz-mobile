45305bd04b9b897b5ddf403cdde94556f2f13b84
Harshit Reddy <reddyharshit8@gmail.com>
2025-08-18 18:30:08 +0530

    refetch is working

--- app/(authenticated)/home.tsx
+++ app/(authenticated)/home.tsx
@@ -60,26 +60,33 @@ const Dashboard = () => {
   const { data: invites, refetch } = useGetInvitations({
     userId: user?.userId,
   });
-  const { data: outgoingInvitesRaw, refetch: refetchOutgoing } = useGetPlayerInvitationSent({
+  const { data: outgoingInvitesRaw, refetch: refetchOutgoing } =
+    useGetPlayerInvitationSent({
       inviteeEmail: user?.email,
     });
   const clubId = user?.currentActiveClubId;
   const userId = user?.userId;
   const openClubId = user?.currentActiveClubId || 'GLOBAL';
-  const { data: openPlayInvites, status , error, refetch:refetchOpenPlay } = useGetPlays(openClubId,userId);
-  const { data: initiatedData, status: initiatedStatus } = useGetInitiatedPlays(userId);
-  
+  const {
+    data: openPlayInvites,
+    status,
+    error,
+    refetch: refetchOpenPlay,
+  } = useGetPlays(openClubId, userId);
+  const { data: initiatedData, status: initiatedStatus } =
+    useGetInitiatedPlays(userId);
+
   // console.log('Open Play Invites:', openPlayInvites);
- 
+
   // ADD THIS LINE - Get the refetch trigger from Redux
   const shouldRefetchInvitations = useSelector(
     (state: RootState) => state.refetch.shouldRefetchInvitations
   );
 
   const [loadingId, setLoadingId] = useState<number | null>(null);
-  const [activeTab, setActiveTab] = useState<'INCOMING' | 'OUTGOING' | 'OPENPLAY'>(
-    'INCOMING'
-  );
+  const [activeTab, setActiveTab] = useState<
+    'INCOMING' | 'OUTGOING' | 'OPENPLAY'
+  >('INCOMING');
   const [selectedInvite, setSelectedInvite] = useState<any>(null);
   const [dialogVisible, setDialogVisible] = useState(false);
   const [comment, setComment] = useState('');
@@ -87,8 +94,9 @@ const Dashboard = () => {
     'accept' | 'reject' | null
   >(null);
 
- 
-  const [playerCounts, setPlayerCounts] = useState<{ [key: string]: { accepted: number; total: number } }>({});
+  const [playerCounts, setPlayerCounts] = useState<{
+    [key: string]: { accepted: number; total: number };
+  }>({});
   const [playerDetailsVisible, setPlayerDetailsVisible] = useState(false);
   const [selectedPlayers, setSelectedPlayers] = useState<any[]>([]);
 
@@ -98,58 +106,56 @@ const Dashboard = () => {
   const outgoingInvites = Object.values(groupedOutgoing);
   // console.log('Outgoing Invites:', outgoingInvites);
   const pendingOutCount = outgoingInvites.length;
-  const playCount = (openPlayInvites?.length || 0) + (initiatedData?.length || 0);
-
+  const playCount =
+    (openPlayInvites?.length || 0) + (initiatedData?.length || 0);
 
   const allInvites = invites ?? [];
   // console.log('All Invites:', allInvites);
   const playCalendarData = [
-  ...(allInvites || []).map(invite => ({
-    ...invite,
-    type: 'incoming' as const,
-  })),
-  ...outgoingInvites.map(invite => ({
-    ...invite,
-    type: 'outgoing' as const,
-  })),
-    ...(openPlayInvites || []).map(play => {
-    const startDate = new Date(
-      play.startTime[0],
-      play.startTime[1] - 1,
-      play.startTime[2],
-      play.startTime[3] || 0,
-      play.startTime[4] || 0
-    );
-    return {
-      type: 'openplay' as const,
-      playTime: play.startTime,
-      placeToPlay: play.allCourts?.Name || 'Unknown Court',
-      dateTimeMs: startDate.getTime(),
-      eventName: play.eventName?.replace(/_/g, ' ') || 'Unknown Play',
-      isWaitlisted: play.waitlistedPlayers?.includes(userId),
-      accepted: play.registeredPlayers?.length ?? 0,
-      playersNeeded: play.maxPlayers ?? 1,
-      id: play.id,
-    };
-  }),
-];
+    ...(allInvites || []).map((invite) => ({
+      ...invite,
+      type: 'incoming' as const,
+    })),
+    ...outgoingInvites.map((invite) => ({
+      ...invite,
+      type: 'outgoing' as const,
+    })),
+    ...(openPlayInvites || []).map((play) => {
+      const startDate = new Date(
+        play.startTime[0],
+        play.startTime[1] - 1,
+        play.startTime[2],
+        play.startTime[3] || 0,
+        play.startTime[4] || 0
+      );
+      return {
+        type: 'openplay' as const,
+        playTime: play.startTime,
+        placeToPlay: play.allCourts?.Name || 'Unknown Court',
+        dateTimeMs: startDate.getTime(),
+        eventName: play.eventName?.replace(/_/g, ' ') || 'Unknown Play',
+        isWaitlisted: play.waitlistedPlayers?.includes(userId),
+        accepted: play.registeredPlayers?.length ?? 0,
+        playersNeeded: play.maxPlayers ?? 1,
+        id: play.id,
+      };
+    }),
+  ];
   useEffect(() => {
     if (isFocused) {
-      refetchOpenPlay(); 
+      refetchOpenPlay();
     }
   }, [isFocused]);
-const getInviteTimestamp = (invite) => {
-  if (invite.dateTimeMs) {
-    return Number(invite.dateTimeMs);
-  }
-  if (Array.isArray(invite.playTime)) {
-    
-    const [year, month, day, hour = 0, minute = 0] = invite.playTime;
-    return new Date(year, month - 1, day, hour, minute).getTime();
-  }
-  return 0;
-};
-
+  const getInviteTimestamp = (invite) => {
+    if (invite.dateTimeMs) {
+      return Number(invite.dateTimeMs);
+    }
+    if (Array.isArray(invite.playTime)) {
+      const [year, month, day, hour = 0, minute = 0] = invite.playTime;
+      return new Date(year, month - 1, day, hour, minute).getTime();
+    }
+    return 0;
+  };
 
   useEffect(() => {
     const loadUser = async () => {
@@ -182,10 +188,9 @@ const getInviteTimestamp = (invite) => {
             }
           );
 
-          const total = res.data[0]?.playersNeeded+1|| 1;
-          const accepted = res.data.filter(
-            (p: any) => p.status === 'ACCEPTED'
-          ).length + 1;
+          const total = res.data[0]?.playersNeeded + 1 || 1;
+          const accepted =
+            res.data.filter((p: any) => p.status === 'ACCEPTED').length + 1;
 
           newCounts[invite.requestId] = { accepted, total };
         } catch (error) {
@@ -243,10 +248,13 @@ const getInviteTimestamp = (invite) => {
   const handleViewPlayers = async (requestId: string) => {
     try {
       const token = await getToken();
-      const res = await axios.get(`${API_URL}/api/player-tracker/tracker/request`, {
-        params: { requestId },
-        headers: { Authorization: `Bearer ${token}` },
-      });
+      const res = await axios.get(
+        `${API_URL}/api/player-tracker/tracker/request`,
+        {
+          params: { requestId },
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
       setSelectedPlayers(res.data);
       //  console.log('Selected Players:', res.data);
       setPlayerDetailsVisible(true);
@@ -261,9 +269,11 @@ const getInviteTimestamp = (invite) => {
         <ScrollView contentContainerStyle={styles.container}>
           <View style={styles.inviteWrapper}>
             <View style={styles.tabRow}>
-              <ScrollView   horizontal
+              <ScrollView
+                horizontal
                 showsHorizontalScrollIndicator={false}
-                contentContainerStyle={styles.chipGroup}>
+                contentContainerStyle={styles.chipGroup}
+              >
                 <Text
                   style={[
                     styles.chip,
@@ -303,14 +313,18 @@ const getInviteTimestamp = (invite) => {
               (activeTab === 'OPENPLAY' && playCount > 0) ? (
                 <TouchableOpacity
                   onPress={() => {
-                      if (activeTab === 'INCOMING') {
-                        router.replace('/(authenticated)/player-invitations?type=incoming');
-                      } else if (activeTab === 'OUTGOING') {
-                        router.replace('/(authenticated)/player-invitations?type=outgoing');
-                      } else if (activeTab === 'OPENPLAY') {
-                        router.replace('/(authenticated)/open-play-viewall');
-                      }
-                    }}
+                    if (activeTab === 'INCOMING') {
+                      router.replace(
+                        '/(authenticated)/player-invitations?type=incoming'
+                      );
+                    } else if (activeTab === 'OUTGOING') {
+                      router.replace(
+                        '/(authenticated)/player-invitations?type=outgoing'
+                      );
+                    } else if (activeTab === 'OPENPLAY') {
+                      router.replace('/(authenticated)/open-play-viewall');
+                    }
+                  }}
                 >
                   <Text style={styles.viewAllText}>View All</Text>
                 </TouchableOpacity>
@@ -331,27 +345,37 @@ const getInviteTimestamp = (invite) => {
                       No incoming invitations
                     </Text>
                   ) : (
-                   [...allInvites]
-                    .sort((a, b) => getInviteTimestamp(a) - getInviteTimestamp(b))
-                    .map((invite) => (
-                      <InvitationCard
-                        key={invite.id}
-                        invite={invite}
-                        onAccept={() => showCommentDialog(invite, 'accept')}
-                        onReject={() => showCommentDialog(invite, 'reject')}
-                        loading={loadingId === invite.id}
-                        totalPlayers={playerCounts[invite.requestId]?.total ?? 1}
-                        acceptedPlayers={playerCounts[invite.requestId]?.accepted ?? 0}
-                        onViewPlayers={handleViewPlayers}
-                      />
-                    ))
+                    [...allInvites]
+                      .sort(
+                        (a, b) => getInviteTimestamp(a) - getInviteTimestamp(b)
+                      )
+                      .map((invite) => (
+                        <InvitationCard
+                          key={invite.id}
+                          invite={invite}
+                          onAccept={() => showCommentDialog(invite, 'accept')}
+                          onReject={() => showCommentDialog(invite, 'reject')}
+                          loading={loadingId === invite.id}
+                          totalPlayers={
+                            playerCounts[invite.requestId]?.total ?? 1
+                          }
+                          acceptedPlayers={
+                            playerCounts[invite.requestId]?.accepted ?? 0
+                          }
+                          onViewPlayers={handleViewPlayers}
+                        />
+                      ))
                   )
                 ) : activeTab === 'OUTGOING' ? (
                   outgoingInvites.length === 0 ? (
-                    <Text style={styles.noInvitesText}>No sent invitations</Text>
+                    <Text style={styles.noInvitesText}>
+                      No sent invitations
+                    </Text>
                   ) : (
                     [...outgoingInvites]
-                      .sort((a, b) => Number(a.dateTimeMs) - Number(b.dateTimeMs))
+                      .sort(
+                        (a, b) => Number(a.dateTimeMs) - Number(b.dateTimeMs)
+                      )
                       .map((invite) => (
                         <OutgoingInviteCardItem
                           key={invite.requestId}
@@ -359,42 +383,42 @@ const getInviteTimestamp = (invite) => {
                           onViewPlayers={handleViewPlayers}
                         />
                       ))
-                    )
+                  )
                 ) : (
                   <OpenPlayCard
                     data={[...openPlayInvites].sort(
                       (a, b) => Number(a.dateTimeMs) - Number(b.dateTimeMs)
                     )}
-                    refetch={refetch}
+                    refetch={refetchOpenPlay}
                   />
                 )}
               </ScrollView>
             </LinearGradient>
           </View>
-        
+
           <FindplayerCard />
 
           <View style={styles.playCalendarHeaderRow}>
-          <Text style={styles.playCalendarHeader}>Play Calendar</Text>
-          <TouchableOpacity onPress={() => router.push('/(authenticated)/play-calendar')}>
-            <Text style={styles.viewAllText}>Play Calendar</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.calendarContainer}>
-          {playCalendarData.length === 0 ? (
-            <Text style={styles.noInvitesText}>No upcoming events</Text>
-          ) : (
-            <ScrollView
-          nestedScrollEnabled
-          contentContainerStyle={styles.calendarContent}
-          >
-              <PlayCalendarCard invites={playCalendarData} />
-              </ScrollView>
-
-            )}
+            <Text style={styles.playCalendarHeader}>Play Calendar</Text>
+            <TouchableOpacity
+              onPress={() => router.push('/(authenticated)/play-calendar')}
+            >
+              <Text style={styles.viewAllText}>Play Calendar</Text>
+            </TouchableOpacity>
           </View>
 
+          <View style={styles.calendarContainer}>
+            {playCalendarData.length === 0 ? (
+              <Text style={styles.noInvitesText}>No upcoming events</Text>
+            ) : (
+              <ScrollView
+                nestedScrollEnabled
+                contentContainerStyle={styles.calendarContent}
+              >
+                <PlayCalendarCard invites={playCalendarData} />
+              </ScrollView>
+            )}
+          </View>
 
           <Portal>
             <Dialog
@@ -432,11 +456,12 @@ const getInviteTimestamp = (invite) => {
           </Portal>
 
           <Text style={styles.playersNearBy}>Players Nearby</Text>
-          <Text style={styles.playersNearByDesc}>See users playing near by</Text>
+          <Text style={styles.playersNearByDesc}>
+            See users playing near by
+          </Text>
           {/* <PlayersNearbyMap /> */}
 
           <NewMessages userId={user?.userId} />
-
         </ScrollView>
       </LinearGradient>
     </PaperProvider>
@@ -518,28 +543,28 @@ const styles = StyleSheet.create({
     color: '#999',
     marginTop: 6,
   },
-bottomModal: {
-  position: 'absolute',
-  bottom: 0,
-  width: '100%',
-  borderTopLeftRadius: 20,
-  borderTopRightRadius: 20,
-  backgroundColor: 'white',
-  padding: 20,
-  elevation: 10,
-  shadowColor: '#000',
-  shadowOffset: { width: 0, height: -2 },
-  shadowOpacity: 0.2,
-  shadowRadius: 5,
-  // height: '50%',
-  // marginTop:400,
-  maxHeight: '90%',
-  marginTop: 'auto',
-  alignSelf: 'stretch',
-},
-dialogContent: {
-  paddingBottom: 20,
-},
+  bottomModal: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: 'white',
+    padding: 20,
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    // height: '50%',
+    // marginTop:400,
+    maxHeight: '90%',
+    marginTop: 'auto',
+    alignSelf: 'stretch',
+  },
+  dialogContent: {
+    paddingBottom: 20,
+  },
   playersNearBy: {
     fontSize: 18,
     fontWeight: '700',
@@ -551,38 +576,34 @@ dialogContent: {
     marginBottom: 20,
   },
   playCalendarHeader: {
-  fontSize: 18,
-  fontWeight: '700',
-  marginBottom: 12,
-},
-calendarContainer: {
-  borderRadius: 16,
-  padding: 5,
-  maxHeight: 240,
-  marginBottom: 20,
-  overflow: 'hidden',
-  shadowOpacity: 0.1,
-  shadowRadius: 4,
-  elevation: 4,
-  backgroundColor: '#fff',
-},
-playCalendarHeaderRow: {
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  marginBottom: 12,
-},
+    fontSize: 18,
+    fontWeight: '700',
+    marginBottom: 12,
+  },
+  calendarContainer: {
+    borderRadius: 16,
+    padding: 5,
+    maxHeight: 240,
+    marginBottom: 20,
+    overflow: 'hidden',
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 4,
+    backgroundColor: '#fff',
+  },
+  playCalendarHeaderRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
 
+  calendarScroll: {
+    borderRadius: 12,
+  },
 
-
-calendarScroll: {
-  borderRadius: 12,
-},
-
-calendarContent: {
-  gap: 10,
-  paddingBottom: 10,
-},
-
-
+  calendarContent: {
+    gap: 10,
+    paddingBottom: 10,
+  },
 });
