21b220d79cf3e77f82ab35bd2f956dcd0f085915
Harshit Reddy <reddyharshit8@gmail.com>
2025-08-28 13:09:50 +0530

    prebuild files

--- ios/kourtwizmobile/AppDelegate.swift
+++ ios/kourtwizmobile/AppDelegate.swift
@@ -2,6 +2,11 @@ import Expo
 import React
 import ReactAppDependencyProvider
 
+// @generated begin react-native-maps-import - expo prebuild (DO NOT MODIFY) sync-bee50fec513f89284e0fa3f5d935afdde33af98f
+#if canImport(GoogleMaps)
+import GoogleMaps
+#endif
+// @generated end react-native-maps-import
 @UIApplicationMain
 public class AppDelegate: ExpoAppDelegate {
   var window: UIWindow?
@@ -29,6 +34,11 @@ public class AppDelegate: ExpoAppDelegate {
       launchOptions: launchOptions)
 #endif
 
+// @generated begin react-native-maps-init - expo prebuild (DO NOT MODIFY) sync-539ecd2e3f9abfa103c23057985c2279ea5dc92f
+#if canImport(GoogleMaps)
+GMSServices.provideAPIKey("AIzaSyC3JM_8P23tGrP6laoq7K8fIGI0DQxNSog")
+#endif
+// @generated end react-native-maps-init
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
